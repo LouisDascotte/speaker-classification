24743d2c976114b3483ec56f3e840a828e41acc0
dan <danial.joukar@student.umons.ac.be>
2019-12-03 13:58:25 +0100

    rule_based() beta

--- main.py
+++ main.py
@@ -42,8 +42,12 @@ def find_distance(x, c):
             elif (secondpoint == None):
                 secondpoint = x[i]
                 break
-    distance = secondpoint - firstpoint
-    return distance
+    if(secondpoint == None or firstpoint ==None):
+        return None
+    else:
+        distance = secondpoint - firstpoint
+        return distance
+
 
 
 def autocorrelation(signal, samplefreq, fmin=50):
@@ -51,9 +55,11 @@ def autocorrelation(signal, samplefreq, fmin=50):
     tab = plt.xcorr(signal, signal,  False, maxlags=n)
     x = tab[0]
     c = tab[1]
-    print(c)
     period = find_distance(x, c)
-    return 1/period
+    if(period == None):
+        return None
+    else:
+        return (1/period)*samplefreq
 
 
 def get_energy(signal):
@@ -64,16 +70,19 @@ def is_voiced(signal, threshold):
     return get_energy(signal) > threshold
 
 
-def file_picker(speaker_name):
+def file_picker(speaker_name, n=5):
     path = glob.glob("cmu_us_"+speaker_name+"_arctic/wav/*.wav")
     files = []
-    for i in random.sample(range(len(path)-1), 5):
+    for i in random.sample(range(len(path)-1), n):
         files.append(path[i])
     return files
 
 
-def find_treshhold(energies):
+def find_treshhold(frames):
     treshhold = None
+    energies = []
+    for frame in frames:
+        energies.append(get_energy(frame))
     for i in range(1, len(energies)-1):
         if(energies[i-1] < energies[i] and energies[i] > energies[i+1]):
             if(treshhold != None):
@@ -83,31 +92,6 @@ def find_treshhold(energies):
                 treshhold = energies[i]
     return treshhold
 
-
-def analyze(filename):
-    datas = wf.read(filename)
-    signal = datas[1]
-    normalized_signal = normalise(signal)
-    frames = split(normalized_signal, 300, 150, datas[0])
-    energies = []
-    for frame in frames:
-        energies.append(get_energy(frame))
-    print(find_treshhold(energies))
-    fig, axs = plt.subplots(1, 2)
-    axs[0].plot(datas[1])
-    axs[0].set_title("signal")
-    axs[1].plot(energies)
-    axs[1].set_title("energy")
-    # plt.plot(datas[1])
-    # plt.plot(energies)
-
-
-def frames_rebuilded(frames):
-    signal = []
-    for frame in frames:
-        for element in frame:
-            signal.append(element)
-    return signal
 
 
 def formants(signal, width, shiftingstep, samplefreq):
@@ -156,12 +140,46 @@ def mfcc(signal,  width, shiftingstep, samplefreq, N_tfd=257):
     return DCT[:13]
 
 
-files = file_picker("slt")
-datas = wf.read(files[0])
-# analyze(files[0])
+def rule_based(width=1000, shiftingstep=1000, sf=1000, N_tfd=257):
+    male = file_picker("bdl", 15)
+    female = file_picker("slt", 15)
+    sentences = []
+    for element in female:
+        sentences.append(element)
+    for element in male:
+        sentences.append(element)
+    random.shuffle(sentences)
+    for sentence in sentences:
+        datas = wf.read(sentence)
+        signal = datas[1]
+        frames = split(signal, width, shiftingstep, sf)
+        treshhold = find_treshhold(frames)
+        pitches = []
+        for frame in frames:
+            if(is_voiced(frame, treshhold)):
+                pitch = autocorrelation(frame, sf)
+                if(pitch != None):
+                    #print("pitch : " + str(pitch))
+                    pitches.append(autocorrelation(frame, sf))
+        #f1 = formants(signal, width, shiftingstep, sf)[0]
+        #print("f1 : "+ str(f1))
+        pitch = np.mean(np.array(pitches))
+        print("pitch : " + str(pitch))
+        if(pitch>350 ):
+            print("the wav file : " + sentence+" is a female voice")
+        else:
+            print("the wav file : " + sentence+" is a male voice")
+
+
+
+
+rule_based()
+#files = file_picker("slt")
+#datas = wf.read(files[0])
+#test(datas[1])
 #freqz = formants(datas[1], 1000, 1000, 1000)
-P_values = mfcc(datas[1], 1000, 1000, 1000)
-print(P_values)
+#P_values = mfcc(datas[1], 1000, 1000, 1000)
+#print(P_values)
 # audio = np.array([1,3,4,65,7,8,8,9,6,4,3,32,12,31,4,45,64,75,-100,-23,-76, 1,3,4,65,7,8,8,9,6,4,3,32,12,31,4,45,64,75,-100,-23,-76, 1,3,4,65,7,8,8,9,6,4,3,32,12,31,4,45,64,75,-100,-23,-76, 1,3,4,65,7,8,8,9,6,4,3,32,12,31,4,45,64,75,-100,-23,-76, 1,3,4,65,7,8,8,9,6,4,3,32,12,31,4,45,64,75,-100,-23,-76, 1,3,4,65,7,8,8,9,6,4,3,32,12,31,4,45,64,75,-100,-23,-76, 1,3,4,65,7,8,8,9,6,4,3,32,12,31,4,45,64,75,-100,-23,-76, 1,3,4,65,7,8,8,9,6,4,3,32,12,31,4,45,64,75,-100,-23,-76, 1,3,4,65,7,8,8,9,6,4,3,32,12,31,4,45,64,75,-100,-23,-76, 1,3,4,65,7,8,8,9,6,4,3,32,12,31,4,45,64,75,-100,-23,-76, 1,3,4,65,7,8,8,9,6,4,3,32,12,31,4,45,64,75,-100,-23,-76], dtype=np.float)
 # normalized_signal = normalise(audio)
 # tab = split(normalized_signal, 1000, 1000, 200)
